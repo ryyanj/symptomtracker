28782c4eb77cc67512df0df975e68259c1925b6b
ryyan smith <ryyanj@gmail.com>
2018-10-06 15:39:33 -0400

    cleaning up

--- src/main/java/com/intrepidjava/symptomtracker/controllers/LoginController.java
+++ src/main/java/com/intrepidjava/symptomtracker/controllers/LoginController.java
@@ -22,13 +22,6 @@ public class LoginController {
     @Autowired
     private MongoUserDetailsService mongoUserDetailsService;
 
-//    @GetMapping("/login")
-//    public ModelAndView login() {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("login");
-//        return modelAndView;
-//    }
-
     @GetMapping("/register")
     public ModelAndView register() {
         ModelAndView modelAndView = new ModelAndView();
