ef61e6f598c27a2aa25c70a5d221f6fa0022ab55
ryyan smith <ryyanj@gmail.com>
2018-10-09 20:58:12 -0400

    update

--- src/main/java/com/intrepidjava/symptomtracker/controllers/ChartController.java
+++ src/main/java/com/intrepidjava/symptomtracker/controllers/ChartController.java
@@ -1,9 +1,13 @@
 package com.intrepidjava.symptomtracker.controllers;
 
+import com.intrepidjava.symptomtracker.models.User;
 import com.intrepidjava.symptomtracker.services.MongoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+import java.util.Set;
 
 
 @RestController
@@ -14,8 +18,11 @@ public class ChartController {
 
 
     @GetMapping("/chart")
-    public ModelAndView getChart() {
+    public ModelAndView getChart(Principal principal) {
         ModelAndView modelAndView = new ModelAndView();
+        User user = mongoUserDetailsService.findByUsername(principal.getName());
+        Set<String> symptoms = user.getSymptoms();
+        modelAndView.addObject("symptoms",symptoms);
         modelAndView.setViewName("chart");
         return modelAndView;
     }
