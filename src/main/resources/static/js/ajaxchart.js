7979eb55b83275a6d20f1685bcbeee67a234cd93
ryyan smith <ryyanj@gmail.com>
2018-10-18 13:50:52 -0400

    update

--- src/main/resources/static/js/ajaxchart.js
+++ src/main/resources/static/js/ajaxchart.js
@@ -9,24 +9,25 @@ function getChartData() {
         url: '/chartData',
         type: 'GET',
         dataType: 'json',
-        success: function(d) {
-            console.log(d.dataSets)
-            new Chart(document.getElementById("line-chart"), {
-              type: 'line',
-              data: {
-                labels: d.labels,
-                datasets: d.dataSets
-              },
-              options: {
-                title: {
-                  display: true,
-                  text: 'symptoms'
-                }
-              }
-            });
+        success: function(chartData) {
+            var config = {
+                                       type: 'line',
+                                       data: {
+                                         labels: chartData.labels,
+                                         datasets: chartData.dataSets
+                                       },
+                                       options: {
+                                         title: {
+                                           display: true,
+                                           text: 'symptoms'
+                                         }
+                                       }
+                                     }
+            new Chart(document.getElementById("line-chart"), config);
         },
         error: function(request, error) {
             alert("Request: " + JSON.stringify(request));
         }
     });
 };
+
