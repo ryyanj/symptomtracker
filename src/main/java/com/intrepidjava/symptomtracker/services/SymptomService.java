5e5999d6bb9a7ca37451cc2a6eaac98ea4cda91d
ryyan smith <ryyanj@gmail.com>
2018-10-21 09:06:08 -0400

    update

--- src/main/java/com/intrepidjava/symptomtracker/services/SymptomService.java
+++ src/main/java/com/intrepidjava/symptomtracker/services/SymptomService.java
@@ -5,6 +5,7 @@ import com.intrepidjava.symptomtracker.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.*;
+import java.util.logging.Logger;
 
 @Service
 public class SymptomService {
@@ -13,13 +14,24 @@ public class SymptomService {
     private UserRepository userRepository;
 
     public void addSymptom(User user, String symptom) {
-        user.getSymptoms().add(symptom);
-        userRepository.save(user);
+        if(symptomHasNotBeenAdded(symptom,user.getSymptoms())) {
+            user.getSymptoms().add(symptom);
+            userRepository.save(user);
+        }
     }
 
     public void removeSymptoms(User user, List<String> symptoms) {
         user.getSymptoms().removeAll(symptoms);
         userRepository.save(user);
+    }
+
+    private boolean symptomHasNotBeenAdded(String symptom, Set<String> symptoms) {
+        List<String> symptomList = new ArrayList<>();
+        symptomList.addAll(symptoms);
+        for(String s: symptomList) {
+            if(symptom.toLowerCase().equals(s.toLowerCase())) return false;
+        }
+        return true;
     }
 
 }
